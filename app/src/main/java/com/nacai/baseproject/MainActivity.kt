2c4866e8f6a70a2525bb83c92969ed97429ee4f6
tyl <simpletang1994@gmail.com>
2021-05-08 18:30:47 +0800

    Provider 改为懒加载

--- app/src/main/java/com/nacai/baseproject/MainActivity.kt
+++ app/src/main/java/com/nacai/baseproject/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import com.nacai.base_lib.base.EventObserver
 import com.nacai.base_lib.base.VMEvent
 import com.nacai.base_lib.base.event
@@ -13,6 +14,9 @@ import com.nacai.base_lib.provider.ProviderManager
 import com.nacai.base_lib.widget.multistate.PageStatus
 import com.nacai.baseproject.databinding.ActivityMainBinding
 import com.nacai.common.provider.MainProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     val state = MutableLiveData<VMEvent<PageStatus>>(event(PageStatus.NORMAL))
@@ -24,12 +28,19 @@ class MainActivity : AppCompatActivity() {
         mBinding.lifecycleOwner = this
         mBinding.aty = this
         findViewById<Button>(R.id.btn).onClick {
-            ProviderManager.get<MainProvider>().show("MainActivity Call")
+//            ProviderManager.get<MainProvider>().show("MainActivity Call")
 //            if (state.value?.peekContent() == PageStatus.ERROR) {
 //                state.value = event(PageStatus.CONTENT)
 //            } else {
 //                state.value = event(PageStatus.ERROR)
 //            }
+            repeat(10){
+                lifecycleScope.launch {
+                    withContext(Dispatchers.IO){
+                        ProviderManager.get<MainProvider>().show("MainActivity Call:$it")
+                    }
+                }
+            }
         }
     }
 }
